08c7486249c2a99155f882dd87646eb4edc792a7
patryk1526 <patryk1526@gmail.com>
2020-03-17 20:41:11 +0100

    Refactorize repairs component

--- PcService-SPA/src/app/repairs/repairs.component.ts
+++ PcService-SPA/src/app/repairs/repairs.component.ts
@@ -3,10 +3,12 @@ import { Repair } from '../_models/repair';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Pagination } from '../_models/pagination';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { RepairsService } from '../_services/repairs/repairs.service';
+import { BsModalRef, BsModalService, PaginationComponent } from 'ngx-bootstrap';
 import { AlertifyService } from '../_services/alertify/alertify.service';
 import { RepairModalComponent } from '../admin/repair-modal/repair-modal.component';
+import { EquipmentsService } from '../_services/equipments/equipments.service';
+import { Equipment } from '../_models/equipment';
+import { EquipmentModalComponent } from '../admin/equipment-modal/equipment-modal.component';
 
 @Component({
   selector: 'app-repairs',
@@ -15,22 +17,18 @@ import { RepairModalComponent } from '../admin/repair-modal/repair-modal.compone
 })
 export class RepairsComponent implements OnInit {
   @Input() admin = false;
-  repairs: Repair[] = [];
-  repairNumberForm: FormGroup;
+  equipments: Equipment[] = [];
+  equipmentNumberForm: FormGroup;
   currentUserId;
   jwtHelper = new JwtHelperService();
-  pageNumber;
-  pageSize;
-  pagination: Pagination;
-  elementNames: string[];
-  resultNames: string[];
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination = new Pagination();
+  statusOptions: string[];
   bsModalRef: BsModalRef;
 
-  constructor(private repairsService: RepairsService, private alertify: AlertifyService, private formBuilder: FormBuilder,
+  constructor(private equipmentsService: EquipmentsService, private alertify: AlertifyService, private formBuilder: FormBuilder,
     private modalService: BsModalService) {
-    this.repairNumberForm = this.formBuilder.group({
-      repairNumber: ''
-    });
   }
 
   ngOnInit() {
@@ -39,32 +37,23 @@ export class RepairsComponent implements OnInit {
     if (!this.admin) {
       this.currentUserId = decodedToken.nameid;
     }
-    this.getRepairs();
+    this.getEquipments(new Object());
     this.createRegisterForm();
-    this.repairsService.getElementNames().subscribe((response: string[]) => {
-      this.elementNames = response;
-    }, error => {
-      this.alertify.error(error);
-    });
-    this.repairsService.getResultOptions().subscribe((response: string[]) => {
-      this.resultNames = response;
-    }, error => {
-      this.alertify.error(error);
-    });
   }
 
-  reloadRepairs(form: FormGroup) {
+  reloadEquipments(form: FormGroup) {
     this.pageNumber = 1;
     const userParams = Object.assign({}, form.value);
-    this.getRepairs(userParams);
+    this.getEquipments(userParams);
   }
 
-  getRepairs(userParams?) {
-    this.repairsService.getRepairsForUser(this.currentUserId, this.pageNumber, this.pageSize, userParams).subscribe((response) => {
-      this.repairs = response.result;
+  getEquipments(userParams?) {
+    this.equipmentsService.getClientEquipments(this.currentUserId, this.pageNumber, this.pageSize, userParams).subscribe((response) => {
+      this.equipments = response.result;
       this.pagination = response.pagination;
       this.pageNumber = this.pagination.currentPage;
       this.pageSize = this.pagination.itemsPerPage;
+      this.getStatusList();
     }, error => {
       this.alertify.error(error);
     });
@@ -73,15 +62,15 @@ export class RepairsComponent implements OnInit {
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
     this.pageNumber = event.page;
-    this.getRepairs();
+    this.getEquipments();
   }
 
-  addRepairModal() {
-    this.bsModalRef = this.modalService.show(RepairModalComponent);
-    this.bsModalRef.content.addNewRepair.subscribe((repair) => {
-      this.repairsService.addRepair(repair).subscribe((repairId) => {
-        this.getRepairs();
-        this.alertify.success('Repair with ID ' + repairId + ' added');
+  addEquipmentModal() {
+    this.bsModalRef = this.modalService.show(EquipmentModalComponent);
+    this.bsModalRef.content.addNewEquipment.subscribe((equipment) => {
+      this.equipmentsService.addEquipment(equipment.value).subscribe(() => {
+        this.getEquipments();
+        this.alertify.success('Repair with ID ' + ' added');
       }, error => {
         this.alertify.error(error);
       });
@@ -90,9 +79,9 @@ export class RepairsComponent implements OnInit {
     });
   }
 
-  assignRepair() {
-    this.repairsService.addRepairToUser(this.currentUserId, this.repairNumberForm.value).subscribe(() => {
-      this.getRepairs();
+  assignClientToEquipment() {
+    this.equipmentsService.assignClientToEquipment(this.equipmentNumberForm.value.equipmentNumber, this.currentUserId).subscribe(() => {
+      this.getEquipments();
       this.alertify.success('Assigned the repair to your account');
     }, error => {
       this.alertify.error(error);
@@ -100,6 +89,14 @@ export class RepairsComponent implements OnInit {
   }
 
   createRegisterForm() {
-    this.repairNumberForm = this.formBuilder.group({ repairNumber: [''] });
+    this.equipmentNumberForm = this.formBuilder.group({ equipmentNumber: [''] });
+  }
+
+  getStatusList() {
+    this.equipmentsService.getStatusList(this.currentUserId).subscribe((response: string[]) => {
+      this.statusOptions = response;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 }
