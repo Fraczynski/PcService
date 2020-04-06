e486d8b826f49b000175620e88560fc4771502ad
patryk1526 <patryk1526@gmail.com>
2020-04-06 21:12:50 +0200

    Extend elements repository

--- PcService.API/Data/Elements/ElementsRepository.cs
+++ PcService.API/Data/Elements/ElementsRepository.cs
@@ -8,107 +8,125 @@ using PcService.API.Models;
 
 namespace PcService.API.Data.Elements
 {
-   public class ElementsRepository : IElementsRepository
-   {
-      private readonly DataContext _context;
-      public ElementsRepository(DataContext context)
-      {
-         _context = context;
-      }
-      public void Add<T>(T entity) where T : class
-      {
-         _context.Add(entity);
-      }
+    public class ElementsRepository : IElementsRepository
+    {
+        private readonly DataContext _context;
+        public ElementsRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
 
-      public void Delete<T>(T entity) where T : class
-      {
-         _context.Remove(entity);
-      }
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
 
-      public async Task<List<Element>> GetEquipmentElements(int equipmentId)
-      {
-         var elements = _context.Elements.Where(e => e.EquipmentId == equipmentId).Include(n => n.Name).Include(s => s.Serviceman);
+        public async Task<List<Element>> GetEquipmentElements(int equipmentId)
+        {
+            var elements = _context.Elements.Where(e => e.EquipmentId == equipmentId).Include(n => n.Name).Include(s => s.Serviceman);
 
-         return await elements.ToListAsync();
+            return await elements.ToListAsync();
 
-      }
+        }
 
-      public async Task<Element> GetElement(int id)
-      {
-         var element = await _context.Elements.FirstOrDefaultAsync(e => e.Id == id);
+        public async Task<Element> GetElement(int id)
+        {
+            var element = await _context.Elements.FirstOrDefaultAsync(e => e.Id == id);
 
-         return element;
-      }
+            return element;
+        }
 
-      public async Task<bool> SaveAll()
-      {
-         return await _context.SaveChangesAsync() > 0;
-      }
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
 
-      public async Task<PagedList<Element>> GetAllElements(UserParams userParams)
-      {
-         var elements = _context.Elements.Include(n => n.Name).Include(s => s.Serviceman).AsQueryable();
+        public async Task<PagedList<Element>> GetAllElements(UserParams userParams)
+        {
+            var elements = _context.Elements.Include(n => n.Name).Include(s => s.Serviceman).AsQueryable();
 
-         elements = filterResults(elements, userParams);
+            elements = filterResults(elements, userParams);
 
-         return await PagedList<Element>.CreateAsync(elements, userParams.PageNumber, userParams.PageSize); ;
-      }
+            return await PagedList<Element>.CreateAsync(elements, userParams.PageNumber, userParams.PageSize); ;
+        }
 
-      private IQueryable<Element> filterResults(IQueryable<Element> elements, UserParams userParams)
-      {
-         elements = elements.OrderByDescending(u => u.Id);
-         if (userParams.EquipmentId != null)
-         {
-            elements = elements.Where(e => e.EquipmentId == userParams.EquipmentId);
-         }
-         if (!string.IsNullOrEmpty(userParams.ServicemanName))
-         {
-            elements = elements.Where(e => e.Serviceman.UserName.ToLower().Contains(userParams.ServicemanName.ToLower()));
-         }
-         if (!string.IsNullOrEmpty(userParams.Name))
-         {
-            elements = elements.Where(r => r.Name.Name.Equals(userParams.Name));
-         }
-         if (!string.IsNullOrEmpty(userParams.Status))
-         {
-            elements = elements.Where(r => r.Status.ToLower().Equals(userParams.Status.ToLower()));
-         }
-         if (!string.IsNullOrEmpty(userParams.Description))
-         {
-            elements = elements.Where(r => r.Description.ToLower().Contains(userParams.Description.ToLower()));
-         }
-         if (userParams.WarrantyRepair != null)
-         {
-            elements = elements.Where(r => r.WarrantyRepair == userParams.WarrantyRepair);
-         }
-         if (userParams.MinNewWarrantyPeriod != null)
-         {
-            elements = elements.Where(r => r.NewWarrantyPeriod >= userParams.MinNewWarrantyPeriod);
-         }
-         if (userParams.MaxNewWarrantyPeriod != null)
-         {
-            elements = elements.Where(r => r.NewWarrantyPeriod <= userParams.MaxNewWarrantyPeriod.GetValueOrDefault().AddHours(24));
-         }
+        public async Task<PagedList<Element>> GetServicemanElements(int servicemanId, UserParams userParams)
+        {
+            var elements = _context.Elements.Where(e => e.ServicemanId == servicemanId).Include(n => n.Name).Include(s => s.Serviceman).AsQueryable();
 
-         if (!string.IsNullOrEmpty(userParams.OrderBy))
-         {
-            switch (userParams.OrderBy.ToLower())
+            elements = filterResults(elements, userParams);
+
+            return await PagedList<Element>.CreateAsync(elements, userParams.PageNumber, userParams.PageSize); ;
+        }
+
+        public async Task<PagedList<Element>> GetUnassignedElements(UserParams userParams)
+        {
+            var elements = _context.Elements.Where(e => e.ServicemanId == null).Include(n => n.Name).Include(s => s.Serviceman).AsQueryable();
+
+            elements = filterResults(elements, userParams);
+
+            return await PagedList<Element>.CreateAsync(elements, userParams.PageNumber, userParams.PageSize); ;
+        }
+
+        private IQueryable<Element> filterResults(IQueryable<Element> elements, UserParams userParams)
+        {
+            elements = elements.OrderByDescending(u => u.Id);
+            if (userParams.EquipmentId != null)
             {
-               case "name":
-                  elements = elements.OrderBy(r => r.Name.Name);
-                  break;
-               case "status":
-                  elements = elements.OrderBy(r => r.Status);
-                  break;
-               case "newwarrantyperiod":
-                  elements = elements.OrderBy(r => r.NewWarrantyPeriod);
-                  break;
-               default:
-                  elements = elements.OrderByDescending(r => r.Id);
-                  break;
+                elements = elements.Where(e => e.EquipmentId == userParams.EquipmentId);
             }
-         }
-         return elements;
-      }
-   }
+            if (!string.IsNullOrEmpty(userParams.ServicemanName))
+            {
+                elements = elements.Where(e => e.Serviceman.UserName.ToLower().Contains(userParams.ServicemanName.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(userParams.Name))
+            {
+                elements = elements.Where(r => r.Name.Name.Equals(userParams.Name));
+            }
+            if (!string.IsNullOrEmpty(userParams.Status))
+            {
+                elements = elements.Where(r => r.Status.ToLower().Equals(userParams.Status.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(userParams.Description))
+            {
+                elements = elements.Where(r => r.Description.ToLower().Contains(userParams.Description.ToLower()));
+            }
+            if (userParams.WarrantyRepair != null)
+            {
+                elements = elements.Where(r => r.WarrantyRepair == userParams.WarrantyRepair);
+            }
+            if (userParams.MinNewWarrantyPeriod != null)
+            {
+                elements = elements.Where(r => r.NewWarrantyPeriod >= userParams.MinNewWarrantyPeriod);
+            }
+            if (userParams.MaxNewWarrantyPeriod != null)
+            {
+                elements = elements.Where(r => r.NewWarrantyPeriod <= userParams.MaxNewWarrantyPeriod.GetValueOrDefault().AddHours(24));
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy.ToLower())
+                {
+                    case "name":
+                        elements = elements.OrderBy(r => r.Name.Name);
+                        break;
+                    case "status":
+                        elements = elements.OrderBy(r => r.Status);
+                        break;
+                    case "newwarrantyperiod":
+                        elements = elements.OrderBy(r => r.NewWarrantyPeriod);
+                        break;
+                    default:
+                        elements = elements.OrderByDescending(r => r.Id);
+                        break;
+                }
+            }
+            return elements;
+        }
+    }
 }
--- PcService.API/Data/Elements/IElementsRepository.cs
+++ PcService.API/Data/Elements/IElementsRepository.cs
@@ -6,13 +6,15 @@ using PcService.API.Models;
 
 namespace PcService.API.Data.Elements
 {
-   public interface IElementsRepository
-   {
-      void Add<T>(T entity) where T : class;
-      void Delete<T>(T entity) where T : class;
-      Task<bool> SaveAll();
-      Task<List<Element>> GetEquipmentElements(int equipmentId);
-      Task<Element> GetElement(int id);
-      Task<PagedList<Element>> GetAllElements(UserParams userParams);
-   }
+    public interface IElementsRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<List<Element>> GetEquipmentElements(int equipmentId);
+        Task<Element> GetElement(int id);
+        Task<PagedList<Element>> GetAllElements(UserParams userParams);
+        Task<PagedList<Element>> GetServicemanElements(int servicemanId, UserParams userParams);
+        Task<PagedList<Element>> GetUnassignedElements(UserParams userParams);
+    }
 }
