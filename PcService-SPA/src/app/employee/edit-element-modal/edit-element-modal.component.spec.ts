8bc2d06bb5d1ef1e1f1e9d26adea0549bed42c78
patryk1526 <patryk1526@gmail.com>
2020-04-06 21:09:42 +0200

    Create edit element modal

---
+++ PcService-SPA/src/app/employee/edit-element-modal/edit-element-modal.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { EditElementModalComponent } from './edit-element-modal.component';
+
+describe('EditElementModalComponent', () => {
+  let component: EditElementModalComponent;
+  let fixture: ComponentFixture<EditElementModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditElementModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditElementModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ PcService-SPA/src/app/employee/edit-element-modal/edit-element-modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ElementsService } from 'src/app/_services/elements/elements.service';
+import { AlertifyService } from 'src/app/_services/alertify/alertify.service';
+import { Element } from 'src/app/_models/element';
+import { BsModalRef } from 'ngx-bootstrap';
+
+@Component({
+  selector: 'app-edit-element-modal',
+  templateUrl: './edit-element-modal.component.html',
+  styleUrls: ['./edit-element-modal.component.css']
+})
+export class EditElementModalComponent implements OnInit {
+  @Output() refreshElements = new EventEmitter();
+  editedElement: Element;
+  elementForm: FormGroup;
+  boolOptions = [true, false];
+
+  constructor(private elementsService: ElementsService, private alertify: AlertifyService, private formBuilder: FormBuilder,
+    public bsModalRef: BsModalRef) { }
+
+  ngOnInit() {
+    this.createElementForm();
+  }
+
+  createElementForm() {
+    this.elementForm = this.formBuilder.group({
+      id: [this.editedElement.id],
+      status: [this.editedElement.status],
+      description: [this.editedElement.description],
+      warrantyRepair: [this.editedElement.warrantyRepair],
+      partsCost: [this.editedElement.partsCost],
+      serviceCost: [this.editedElement.serviceCost],
+      newWarrantyPeriod: [this.editedElement.newWarrantyPeriod]
+    });
+  }
+
+  updateElement() {
+    this.elementsService.updateElement(this.elementForm.value).subscribe(() => {
+      this.alertify.success('Updated');
+      this.refreshElements.emit(null);
+      this.bsModalRef.hide();
+    }, error => {
+      console.log(error);
+      this.alertify.error(error);
+    });
+  }
+
+}
