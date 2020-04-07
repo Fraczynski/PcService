0958b7c4f40bb7a99ddec344e9e99e1a9d3cf878
patryk1526 <patryk1526@gmail.com>
2020-04-07 21:37:38 +0200

    Fix users repository

--- PcService.API/Data/Users/IUsersRepository.cs
+++ PcService.API/Data/Users/IUsersRepository.cs
@@ -4,10 +4,13 @@ using PcService.API.Models;
 
 namespace PcService.API.Data.Users
 {
-   public interface IUsersRepository
-   {
-      Task<List<User>> GetUsers();
-      Task<User> GetUser(int id);
-      Task<User> GetUser(string userName);
-   }
+    public interface IUsersRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<List<User>> GetUsers();
+        Task<User> GetUser(int id);
+        Task<User> GetUser(string userName);
+    }
 }
--- PcService.API/Data/Users/UsersRepository.cs
+++ PcService.API/Data/Users/UsersRepository.cs
@@ -5,32 +5,48 @@ using PcService.API.Models;
 
 namespace PcService.API.Data.Users
 {
-   public class UsersRepository : IUsersRepository
-   {
-      private readonly DataContext _context;
-      public UsersRepository(DataContext context)
-      {
-         _context = context;
-      }
-      public async Task<User> GetUser(int id)
-      {
-         var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly DataContext _context;
+        public UsersRepository(DataContext context)
+        {
+            _context = context;
+        }
 
-         return user;
-      }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
 
-      public async Task<User> GetUser(string userName)
-      {
-         var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
 
-         return user;
-      }
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
 
-      public async Task<List<User>> GetUsers()
-      {
-         var users = await _context.Users.ToListAsync();
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
 
-         return users;
-      }
-   }
+            return user;
+        }
+
+        public async Task<User> GetUser(string userName)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+    }
 }
